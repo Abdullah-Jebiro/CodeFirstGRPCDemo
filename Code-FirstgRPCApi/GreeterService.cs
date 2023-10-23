a8a65a5fbb9083f0bb44a6bc807c8f691937558b
Abdullah Jebiro <a.jebiro@manteq-me.com>
2023-10-23 17:37:54 +0300

    Add project files.

---
+++ Code-FirstgRPCApi/GreeterService.cs
@@ -0,0 +1,23 @@
+﻿using ProtoBuf.Grpc;
+using Shared.Contracts;
+
+public class GreeterService : IGreeterService
+{
+    //public Task<Response> SayHelloAsync(Request request, CallContext context = default)
+    //{
+    //    return Task.FromResult(
+    //            new Response
+    //            {
+    //                Message = $"Hello {request.Name}"
+    //            });
+    //}
+
+    public Task<Response> SayHelloAsync(Request request)
+    {
+        return Task.FromResult(
+                new Response
+                {
+                    Message = $"Hello {request.Name.ToUpper()}"
+                });
+    }
+}
---
+++ Common/Class.cs
@@ -0,0 +1,27 @@
+﻿using ProtoBuf.Grpc;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace Shared.Contracts;
+
+[DataContract]
+public class Response
+{
+    [DataMember(Order = 1)]
+    public string Message { get; set; }
+}
+
+[DataContract]
+public class Request
+{
+    [DataMember(Order = 1)]
+    public string Name { get; set; }
+}
+
+[ServiceContract]
+public interface IGreeterService
+{
+    [OperationContract]
+    Task<Response> SayHelloAsync(Request request);
+}
