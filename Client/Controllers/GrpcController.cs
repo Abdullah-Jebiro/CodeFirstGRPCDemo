49d48696358d6653ca1f80f2f3f9dea75ff473e1
Abdullah-Jebiro <abdullahjebiro@gmail.com>
2025-03-09 04:24:41 +0300

    update

--- Client/Controllers/GrpcController.cs
+++ Client/Controllers/GrpcController.cs
@@ -5,19 +5,12 @@ using System.Threading.Tasks;
 
 [ApiController]
 [Route("api/grpc")]
-public class GrpcController : ControllerBase
+public class GrpcController(IGreeterService greeterService) : ControllerBase
 {
-    private readonly IGreeterService _greeterService;
-
-    public GrpcController(IGreeterService greeterService)
-    {
-        _greeterService = greeterService;
-    }
-
     [HttpGet("say-hello")]
     public async Task<IActionResult> SayHello(string name)
     {
-        var response = await _greeterService.SayHelloAsync(new Request { Name = name });
+        var response = await greeterService.SayHelloAsync(new Request { Name = name });
         return Ok(response);
     }
 }
